5009b367d48352bbcdc962f2df1f10a5c2afc840
iomegak12 <jd.ramkumar@gmail.com>
2022-03-01 23:40:31 +0530

    updated commites

---
+++ .env.cmd
@@ -0,0 +1,4 @@
+@echo off
+set ORDERS_DATA_FOLDER=./data/orders/
+set CUSTOMERS_SERVICE_URL=https://621e3ecf849220b1fc942bac.mockapi.io/customers
+set PRODUCTS_DATA_FILE=./data/products.csv
---
+++ .env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+export ORDERS_DATA_FOLDER=./data/orders/
+export CUSTOMERS_SERVICE_URL=https://621e3ecf849220b1fc942bac.mockapi.io/customers
+export PRODUCTS_DATA_FILE=./data/products.csv
